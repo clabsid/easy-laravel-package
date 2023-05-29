e78fae91e27b8b40b9d7d114dfe5c3c6482c5570
unknown <imho.production@gmail.com>
2023-05-29 23:45:55 +0800

    test

--- src/Traits/GenerateKode.php
+++ src/Traits/GenerateKode.php
@@ -5,46 +5,51 @@ namespace EasyLaravelPackage\Traits;
 
 trait GenerateKode
 {
-    /**
-     * Boot function from Laravel.
-     */
-    protected static function boot()
-    {
-        parent::boot();
-        static::creating(function ($model) {
-            if (empty($model->{$model->getKeyName()})) {
-                $model->{$model->getKeyName()} = $model->uid();
-            }
-        });
-    }
+  /**
+   * Boot function from Laravel.
+   */
+  protected static function boot()
+  {
+    parent::boot();
+    static::creating(function ($model) {
+      if (empty($model->{$model->getKeyName()})) {
+        $model->{$model->getKeyName()} = $model->uid();
+      }
+    });
+  }
 
-    /**
-     * Get the value indicating whether the IDs are incrementing.
-     *
-     * @return bool
-     */
-    public function getIncrementing()
-    {
-        return false;
-    }
+  /**
+   * Get the value indicating whether the IDs are incrementing.
+   *
+   * @return bool
+   */
+  public function getIncrementing()
+  {
+    return false;
+  }
 
-    /**
-     * Get the auto-incrementing key type.
-     *
-     * @return string
-     */
-    public function getKeyType()
-    {
-        return 'string';
-    }
+  /**
+   * Get the auto-incrementing key type.
+   *
+   * @return string
+   */
+  public function getKeyType()
+  {
+    return 'string';
+  }
 
-    /**
-     * gen uid
-     * @param int $limit
-     * @return false|string
-     */
-    public function uid($limit = 16)
-    {
-        return substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, $limit);
-    }
+  /**
+   * gen uid
+   * @param int $limit
+   * @return false|string
+   */
+  public function uid($limit = 16)
+  {
+    return substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, $limit);
+  }
+
+  public function kode()
+  {
+    return 'test';
+  }
 }
